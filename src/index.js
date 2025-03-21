88e2833b7b3c9a9d61b65a6327113a199e041094
kekedezha <dezhac6@gmail.com>
2025-03-21 14:27:36 -0400

    set up basic get route to test func, installed cors package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Home route");
+});
+
 console.log(
   "Hello! This project will be my ecomm back end REST API for a fitness brand!"
 );
