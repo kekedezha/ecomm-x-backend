e09aaad28ecec5947b8261fac9f20f929846c4ad
kekedezha <dezhac6@gmail.com>
2025-03-21 14:14:55 -0400

    initail set up for express backend server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import express from "express";
+
+const app = express();
+
 console.log(
   "Hello! This project will be my ecomm back end REST API for a fitness brand!"
 );
 console.log("I just installed and added nodemon as a project dependency");
+
+app.listen(3000, () => {
+  console.log("Listening! Initial set up for backend server.");
+});
