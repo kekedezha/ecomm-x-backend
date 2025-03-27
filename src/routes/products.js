36a71c6448996ab7489ce8e30f37ccbe8952ca57
kekedezha <dezhac6@gmail.com>
2025-03-27 11:42:11 -0400

    udpated file to import route functions from corresponding controller file

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,38 +1,29 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
+import {
+  getAllProducts,
+  getProductById,
+  addNewProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/products.controller";
 
 // Initialize a router instance to use with 'products' routes
 const router = Router();
 
 // GET HTTP route for retrieving all of the products from the db
-router.get("/", (req, res) => {
-  return res.send("General GET HTTP method on products resource");
-});
+router.get("/", getAllProducts);
 
 // GET HTTP route for getting a single product from the db
-router.get("/:productId", (req, res) => {
-  return res.send(
-    `GET HTTP method on products resource for products/${req.params.productId}`
-  );
-});
+router.get("/:productId", getProductById);
 
 // POST HTTP route for creating a new product to the db
-router.post("/", (req, res) => {
-  return res.status(201).send("POST HTTP method on products resource");
-});
+router.post("/", addNewProduct);
 
-// PUT HTTP route for updating a product info
-router.put("/:productId", (req, res) => {
-  return res.send(
-    `PUT HTTP method on products resource for products/${req.params.productId}`
-  );
-});
+// PUT HTTP route for updating a product
+router.put("/:productId", updateProduct);
 
 // DELETE HTTP route for deleting a product
-router.delete("/:productId", (req, res) => {
-  return res.send(
-    `DELETE HTTP method on products resource for products/${req.params.productId}`
-  );
-});
+router.delete("/:productId", deleteProduct);
 
 export default router;
