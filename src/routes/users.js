25955582e12c5f3f9214bc6270b7e730db0fa84c
kekedezha <dezhac6@gmail.com>
2025-04-07 16:52:02 -0400

    added one new path for logging in a user and modified previous POST route for registering a new user

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,8 @@ import { Router } from "express";
 import {
   getAllUsers,
   getUserById,
-  addNewUser,
+  registerNewUser,
+  loginUser,
   updateUser,
   deleteUser,
 } from "../controllers/users.controller";
@@ -11,14 +12,17 @@ import {
 // Initialize a router instance to use with 'users' routes
 const router = Router();
 
-// GET HTTP route for retrieving all of the users from the db
-router.get("/", getAllUsers);
+// GET HTTP route for retrieving all of the users from the db - ADMIN ONLY
+router.get("/admin", getAllUsers);
 
 // GET HTTP route for getting a single user from the db
 router.get("/:userId", getUserById);
 
 // POST HTTP route for creating a new user to the db
-router.post("/", addNewUser);
+router.post("/register", registerNewUser);
+
+// POST HTTP route for logging in an existing user
+router.post("/login", loginUser);
 
 // PUT HTTP route for updating a users info
 router.put("/:userId", updateUser);
