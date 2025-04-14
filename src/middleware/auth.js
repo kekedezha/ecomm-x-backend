d91f154d1e98c90cdd30626d1dddf9a0830cb56c
kekedezha <dezhac6@gmail.com>
2025-04-14 17:20:45 -0400

    added in notes for myself

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,12 +7,26 @@ export const authenticateToken = (req, res, next) => {
 
   if (!token) return res.status(401).json({ error: "User is unauthorized." });
 
+  /**
+   * JWT payload from jwt.verify()
+   * -----------------------------
+   * jwt payload = {
+   *  id: 'xxxx'
+   *  username: 'xxxx'
+   * }
+   */
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err)
-      return res
-        .status(403)
-        .json({ error: "Forbidden. JWT invalid or expired." });
+    if (err) return res.status(403).json({ error: err });
     req.user = user; // Attach user info to request
+    /**
+     * user = {
+     *  id: 'xxxx'
+     *  username: 'xxxx'
+     *  iat: 999999  // issued at
+     *  eat: 999999  // expires at
+     * }
+     */
     next(); // Proceed to route handler
   });
 };
