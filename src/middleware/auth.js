3a7236bb7ca1260c42eb49138ba8d780828039d8
kekedezha <dezhac6@gmail.com>
2025-04-14 17:33:07 -0400

    modified notes to add role

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,8 +11,9 @@ export const authenticateToken = (req, res, next) => {
    * JWT payload from jwt.verify()
    * -----------------------------
    * jwt payload = {
-   *  id: 'xxxx'
-   *  username: 'xxxx'
+   *  id: 'xxxx',
+   *  username: 'xxxx',
+   *  role: 'user',
    * }
    */
 
@@ -21,10 +22,11 @@ export const authenticateToken = (req, res, next) => {
     req.user = user; // Attach user info to request
     /**
      * user = {
-     *  id: 'xxxx'
-     *  username: 'xxxx'
-     *  iat: 999999  // issued at
-     *  eat: 999999  // expires at
+     *  id: 'xxxx',
+     *  username: 'xxxx',
+     *  role: 'user',
+     *  iat: 999999  // issued at,
+     *  eat: 999999  // expires at,
      * }
      */
     next(); // Proceed to route handler
