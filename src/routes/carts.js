bbfc4bc8537a97e2d7023d6cc3761845a06d14c0
kekedezha <dezhac6@gmail.com>
2025-04-16 17:51:08 -0400

    modified carts routes that will be handled

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -4,35 +4,19 @@ import { Router } from "express";
 // Initialize a router instance to use with 'carts' routes
 const router = Router();
 
-// GET HTTP route for retrieving all of the carts from the db
-router.get("/", (req, res) => {
-  return res.send("General GET HTTP method on carts resource");
-});
+// GET HTTP route for retrieving users cart
+router.get("/:userId");
 
-// GET HTTP route for getting a single cart from the db
-router.get("/:cartId", (req, res) => {
-  return res.send(
-    `GET HTTP method on carts resource for carts/${req.params.cartId}`
-  );
-});
+// POST HTTP route for adding a product to the cart
+router.post("/:userId");
 
-// POST HTTP route for creating a new cart to the db
-router.post("/", (req, res) => {
-  return res.status(201).send("POST HTTP method on carts resource");
-});
+// PUT HTTP route for updating quantity of a product in the cart
+router.put("/:userId/:productId");
 
-// PUT HTTP route for updating a cart
-router.put("/:cartId", (req, res) => {
-  return res.send(
-    `PUT HTTP method on carts resource for carts/${req.params.cartId}`
-  );
-});
+// DELETE HTTP route for removing individual items from cart
+router.delete("/:userId/:productId");
 
-// DELETE HTTP route for deleting a cart
-router.delete("/:cartId", (req, res) => {
-  return res.send(
-    `DELETE HTTP method on carts resource for carts/${req.params.cartId}`
-  );
-});
+// DELETE HTTP route for clearing cart
+router.delete("/:userId");
 
 export default router;
