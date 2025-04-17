b8a9e6afec3b9b261a4c86c983918ee867e9da2c
kekedezha <dezhac6@gmail.com>
2025-04-17 10:07:51 -0400

    modified orders routes that will be handled

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,35 +4,19 @@ import { Router } from "express";
 // Initialize a router instance to use with 'orders' routes
 const router = Router();
 
-// GET HTTP route for retrieving all of the orders from the db
-router.get("/", (req, res) => {
-  return res.send("General GET HTTP method on orders resource");
-});
+// GET HTTP route for retrieving all of the orders for specified user
+router.get("/:userId");
 
-// GET HTTP route for getting a single order from the db
-router.get("/:orderId", (req, res) => {
-  return res.send(
-    `GET HTTP method on orders resource for orders/${req.params.orderId}`
-  );
-});
+// GET HTTP route for getting a specified order for a specified user
+router.get("/:userId/:orderId");
 
-// POST HTTP route for creating a new order to the db
-router.post("/", (req, res) => {
-  return res.status(201).send("POST HTTP method on orders resource");
-});
+// POST HTTP route for creating an order from the cart
+router.post("/:userId");
 
-// PUT HTTP route for updating order
-router.put("/:orderId", (req, res) => {
-  return res.send(
-    `PUT HTTP method on orders resource for orders/${req.params.orderId}`
-  );
-});
+// PUT HTTP route for updating order status
+router.put("/:userId/:orderId");
 
-// DELETE HTTP route for deleting an order
-router.delete("/:orderId", (req, res) => {
-  return res.send(
-    `DELETE HTTP method on orders resource for orders/${req.params.orderId}`
-  );
-});
+// DELETE HTTP route for deleting/canceling an order
+router.delete("/:userId/:orderId");
 
 export default router;
