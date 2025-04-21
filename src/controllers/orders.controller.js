629e6341d0c3ded3977573402f8d53fc16f82b0e
kekedezha <dezhac6@gmail.com>
2025-04-21 17:27:00 -0400

    added skeleton function for order endpoints

---
+++ src/controllers/orders.controller.js
@@ -0,0 +1,31 @@
+import pool from "../db";
+
+// GET function for retrieving all orders for user
+export const getAllUserOrders = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// GET function for retrieving specific order for specific user
+export const getOrderForUser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// POST function for creating an order from the cart
+export const createOrder = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// PUT function for updating order status
+export const updateOrderStatus = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// DELETE function to delete/cancel an order
+export const deleteUserOrder = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,22 +1,29 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
+import {
+  getAllUserOrders,
+  getOrderForUser,
+  createOrder,
+  updateOrderStatus,
+  deleteUserOrder,
+} from "../controllers/orders.controller";
 
 // Initialize a router instance to use with 'orders' routes
 const router = Router();
 
 // GET HTTP route for retrieving all of the orders for specified user
-router.get("/:userId");
+router.get("/:userId", getAllUserOrders);
 
 // GET HTTP route for getting a specified order for a specified user
-router.get("/:userId/:orderId");
+router.get("/:userId/:orderId", getOrderForUser);
 
 // POST HTTP route for creating an order from the cart
-router.post("/:userId");
+router.post("/:userId", createOrder);
 
 // PUT HTTP route for updating order status
-router.put("/:userId/:orderId");
+router.put("/:userId/:orderId", updateOrderStatus);
 
 // DELETE HTTP route for deleting/canceling an order
-router.delete("/:userId/:orderId");
+router.delete("/:userId/:orderId", deleteUserOrder);
 
 export default router;
