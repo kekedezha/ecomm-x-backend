39c5afdac356bbdc39d20ee7e7258a371af52d19
kekedezha <dezhac6@gmail.com>
2025-04-21 17:16:58 -0400

    added skeleton function for cart endpoints

---
+++ src/controllers/carts.controller.js
@@ -0,0 +1,31 @@
+import pool from "../db";
+
+// GET function to retrieve current cart for user
+export const getUsersCurrentCart = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// POST function to add a product to the cart
+export const addProductToCart = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// PUT function to update quantity of a product in the cart
+export const updateProductQty = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// DELETE function to remove individual items from cart
+export const deleteProdFromCart = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// DELETE function to clear current cart
+export const clearCart = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,22 +1,29 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
+import {
+  getUsersCurrentCart,
+  addProductToCart,
+  updateProductQty,
+  deleteProdFromCart,
+  clearCart,
+} from "../controllers/carts.controller";
 
 // Initialize a router instance to use with 'carts' routes
 const router = Router();
 
 // GET HTTP route for retrieving users cart
-router.get("/:userId");
+router.get("/:userId", getUsersCurrentCart);
 
 // POST HTTP route for adding a product to the cart
-router.post("/:userId");
+router.post("/:userId", addProductToCart);
 
 // PUT HTTP route for updating quantity of a product in the cart
-router.put("/:userId/:productId");
+router.put("/:userId/:productId", updateProductQty);
 
 // DELETE HTTP route for removing individual items from cart
-router.delete("/:userId/:productId");
+router.delete("/:userId/:productId", deleteProdFromCart);
 
 // DELETE HTTP route for clearing cart
-router.delete("/:userId");
+router.delete("/:userId", clearCart);
 
 export default router;
