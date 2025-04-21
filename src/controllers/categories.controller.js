47493224dcfdb7e631942fa2fa32f48ac1f4354f
kekedezha <dezhac6@gmail.com>
2025-04-21 17:55:20 -0400

    added skeleton function for categories endpoints

---
+++ src/controllers/categories.controller.js
@@ -0,0 +1,31 @@
+import pool from "../db";
+
+// GET function to list all categories
+export const getAllCategories = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// GET function to get all products in a category
+export const getProdsByCategory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// POST function to create a new category -- ADMIN ONLY
+export const createNewCategory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// PUT function to update a category -- ADMIN ONLY
+export const updateCategory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+// DELETE function to delete a category -- ADMIN ONLY
+export const deleteCategory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,38 +1,29 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
+import {
+  getAllCategories,
+  getProdsByCategory,
+  createNewCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categories.controller";
 
 // Initialize a router instance to use with 'categories' routes
 const router = Router();
 
 // GET HTTP route for retrieving all of the categories from the db
-router.get("/", (req, res) => {
-  return res.send("General GET HTTP method on categories resource");
-});
+router.get("/", getAllCategories);
 
 // GET HTTP route for getting a single category from the db
-router.get("/:categoryId", (req, res) => {
-  return res.send(
-    `GET HTTP method on categories resource for categories/${req.params.categoryId}`
-  );
-});
+router.get("/:categoryId/products", getProdsByCategory);
 
 // POST HTTP route for creating a new category to the db
-router.post("/", (req, res) => {
-  return res.status(201).send("POST HTTP method on categories resource");
-});
+router.post("/", createNewCategory);
 
 // PUT HTTP route for updating a category
-router.put("/:categoryId", (req, res) => {
-  return res.send(
-    `PUT HTTP method on categories resource for categories/${req.params.categoryId}`
-  );
-});
+router.put("/:categoryId", updateCategory);
 
 // DELETE HTTP route for deleting a category
-router.delete("/:categoryId", (req, res) => {
-  return res.send(
-    `DELETE HTTP method on categories resource for categories/${req.params.categoryId}`
-  );
-});
+router.delete("/:categoryId", deleteCategory);
 
 export default router;
