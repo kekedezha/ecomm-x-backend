8b8dbf0424bc3130649ec10c255b7beb52ac4828
kekedezha <dezhac6@gmail.com>
2025-04-21 17:35:16 -0400

    added skeleton function for checkout endpoints

---
+++ src/controllers/checkout.controller.js
@@ -0,0 +1,7 @@
+import pool from "../db";
+
+// POST function to process a payment and finalize an order
+export const finalizeOrder = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,8 +1,9 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
+import { finalizeOrder } from "../controllers/checkout.controller";
 
 // Initialize a router instance to use with 'carts' routes
 const router = Router();
 
 // POST HTTP route to process a payment and finalize an order
-router.post("/:userId");
+router.post("/:userId", finalizeOrder);
