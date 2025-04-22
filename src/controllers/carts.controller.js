85707b9fdb90b75a1047eba2116ca63638b4a45e
kekedezha <dezhac6@gmail.com>
2025-04-22 17:59:53 -0400

    added in middleware to endpoints and filled in catch blocks

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -3,29 +3,44 @@ import pool from "../db";
 // GET function to retrieve current cart for user
 export const getUsersCurrentCart = async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error fetching cart: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
 
 // POST function to add a product to the cart
 export const addProductToCart = async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error adding product to cart: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
 
 // PUT function to update quantity of a product in the cart
 export const updateProductQty = async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error updating cart: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
 
 // DELETE function to remove individual items from cart
 export const deleteProdFromCart = async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error deleting product from cart: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
 
 // DELETE function to clear current cart
 export const clearCart = async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error clearing cart: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 };
--- src/routes/carts.js
+++ src/routes/carts.js
@@ -7,23 +7,34 @@ import {
   deleteProdFromCart,
   clearCart,
 } from "../controllers/carts.controller";
+import { authenticateToken, isSameUser } from "../middleware/auth";
 
 // Initialize a router instance to use with 'carts' routes
 const router = Router();
 
 // GET HTTP route for retrieving users cart
-router.get("/:userId", getUsersCurrentCart);
+router.get("/:userId", authenticateToken, isSameUser, getUsersCurrentCart);
 
 // POST HTTP route for adding a product to the cart
-router.post("/:userId", addProductToCart);
+router.post("/:userId", authenticateToken, isSameUser, addProductToCart);
 
 // PUT HTTP route for updating quantity of a product in the cart
-router.put("/:userId/:productId", updateProductQty);
+router.put(
+  "/:userId/:productId",
+  authenticateToken,
+  isSameUser,
+  updateProductQty
+);
 
 // DELETE HTTP route for removing individual items from cart
-router.delete("/:userId/:productId", deleteProdFromCart);
+router.delete(
+  "/:userId/:productId",
+  authenticateToken,
+  isSameUser,
+  deleteProdFromCart
+);
 
 // DELETE HTTP route for clearing cart
-router.delete("/:userId", clearCart);
+router.delete("/:userId", authenticateToken, isSameUser, clearCart);
 
 export default router;
