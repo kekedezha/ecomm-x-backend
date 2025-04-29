899e4c4873a3ddf3eb5586ca00b5d530f1879e17
kekedezha <dezhac6@gmail.com>
2025-04-29 09:16:49 -0400

    added in catch in case request does not have productID

--- src/middleware/cart.middleware.js
+++ src/middleware/cart.middleware.js
@@ -2,12 +2,17 @@ import pool from "../db";
 
 export const isValidProduct = async (req, res, next) => {
   try {
+    if (!req.body.productId) {
+      return res
+        .status(404)
+        .json({ error: "Bad Request. No product id given." });
+    }
     const productId = parseInt(req.body.productId);
     const result = await pool.query("SELECT * FROM products WHERE id = $1", [
       productId,
     ]);
     if (result.rows.length == 0) {
-      res.status(404).json({ error: "Product does not exist." });
+      return res.status(404).json({ error: "Product does not exist." });
     }
     req.productId = parseInt(result.rows[0].id, 10);
     req.quantity = parseInt(req.body.quantity);
