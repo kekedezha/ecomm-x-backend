ced73c8e3480180dbd865bde1b03abcda70c6558
kekedezha <dezhac6@gmail.com>
2025-04-29 17:30:18 -0400

    added helper file for orders endpoint

---
+++ src/helpers/orders.helper.js
@@ -0,0 +1,18 @@
+import pool from "../db";
+
+// Function to check if the order_id exists for the user requesting it
+export const doesOrderExistForUser = async (userId, orderId) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM orders WHERE user_id = $1 AND id = $2",
+      [userId, orderId]
+    );
+    if (result.rows.length == 0) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.log("Error checking if order exists for user.", error);
+    throw error;
+  }
+};
