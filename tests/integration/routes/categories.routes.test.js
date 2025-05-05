b7ce1dbb0c3c705d500cc78c90304eea4c683c91
kekedezha <dezhac6@gmail.com>
2025-05-05 11:22:31 -0400

    updated testing file for GET method to get all prods on category id

--- tests/integration/routes/categories.routes.test.js
+++ tests/integration/routes/categories.routes.test.js
@@ -32,29 +32,52 @@ describe("Categories endpoints", () => {
     });
   });
 
-  describe.skip("GET HTTP method to retrieve all products by category", () => {
-    it("GET /categories/:categoryId should show categories with specified categoryId", async () => {
-      const res = await requestWithSupertest.get("/categories/:1");
+  describe("GET HTTP method to retrieve all products by category", () => {
+    it("GET /categories/:categoryId/products should show products with specified categoryId", async () => {
+      const res = await requestWithSupertest.get("/categories/1/products");
       expect(res.status).toEqual(200);
-      expect(res.text).toEqual(
-        "GET HTTP method on categories resource for categories/:1"
+      expect(res.type).toEqual(expect.stringContaining("json"));
+      expect(res.body.products).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(String),
+            stock: expect.any(Number),
+            category_id: expect.any(Number),
+          }),
+        ])
       );
+    });
+
+    it("GET /categories/bread/products should fail to show products with an invalid category id", async () => {
+      const res = await requestWithSupertest.get("/categories/bread/products");
+      expect(res.status).toEqual(400);
+      expect(res.type).toEqual(expect.stringContaining("json"));
+      expect(res.body.error).toEqual("Bad Request. Invalid Category ID.");
+    });
+
+    it("GET /categories/999/products should fail show products because the category id was not found", async () => {
+      const res = await requestWithSupertest.get("/categories/999/products");
+      expect(res.status).toEqual(404);
+      expect(res.type).toEqual(expect.stringContaining("json"));
+      expect(res.body.error).toEqual("Category not found.");
     });
   });
 
   // POST REQUEST TESTS
   describe.skip("POST HTTP method to add a new category to the db", () => {
-    it("POST /categories should create a new category to the categories table", async () => {
-      const res = await requestWithSupertest.post("/categories");
+    it("POST /categories/admin should create a new category to the categories table", async () => {
+      const res = await requestWithSupertest.post("/categories/admin");
       expect(res.status).toEqual(201);
       expect(res.text).toEqual("POST HTTP method on categories resource");
     });
   });
 
   // PUT REQUEST TESTS
-  describe.skip("PUT HTTP method to update a cateogry", () => {
-    it("PUT /categories/:categoryId should update categories with specified categoryId", async () => {
-      const res = await requestWithSupertest.put("/categories/:2");
+  describe.skip("PUT HTTP method to update a category", () => {
+    it("PUT /categories/admin/:categoryId should update categories with specified categoryId", async () => {
+      const res = await requestWithSupertest.put("/categories/admin/2");
       expect(res.status).toEqual(200);
       expect(res.text).toEqual(
         "PUT HTTP method on categories resource for categories/:2"
@@ -64,8 +87,8 @@ describe("Categories endpoints", () => {
 
   // DELETE REQUEST TESTS
   describe.skip("DELETE HTTP method to delete a category from the db", () => {
-    it("DELETE /categories/:categoryId should delete categories with specified categoryId", async () => {
-      const res = await requestWithSupertest.delete("/categories/:3");
+    it("DELETE /categories/admin/:categoryId should delete categories with specified categoryId", async () => {
+      const res = await requestWithSupertest.delete("/categories/admin/3");
       expect(res.status).toEqual(200);
       expect(res.text).toEqual(
         "DELETE HTTP method on categories resource for categories/:3"
