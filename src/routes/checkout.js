6d12fd0e3615b9bb272225e35336b047691b80f8
kekedezha <dezhac6@gmail.com>
2025-05-07 12:30:32 -0400

    updated checkout endpoint along with checkout controller function

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,11 +1,12 @@
 // Import 'Router' class from the express module to create modular route handlers
 import { Router } from "express";
 import { finalizeOrder } from "../controllers/checkout.controller";
+import { authenticateToken, isSameUser } from "../middleware/auth";
 
 // Initialize a router instance to use with 'carts' routes
 const router = Router();
 
 // POST HTTP route to process a payment and finalize an order
-router.post("/:userId", finalizeOrder);
+router.post("/:userId", authenticateToken, isSameUser, finalizeOrder);
 
 export default router;
