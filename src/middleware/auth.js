251e5f0b19f4ef9c1a8c805725d8dcdd2ba244e3
kekedezha <dezhac6@gmail.com>
2025-05-14 11:50:32 -0400

    updated error message once again to return correct error

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,10 +4,10 @@ import "dotenv/config";
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader?.split(" ")[1]; // Split string 'Bearer <token>'
-  if (!token) return res.status(401).json({ error: "Invalid token." });
+  if (!token) return res.status(401).json({ error: "Missing token." });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: err });
+    if (err) return res.status(403).json({ error: "Invalid token." });
     req.user = user; // Attach user info to request
     next(); // Proceed to route handler
   });
